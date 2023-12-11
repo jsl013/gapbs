bf74000b6f5e048ff0a77323a150c85f2c2d6fde
jsl013 <junseo.lee@snu.ac.kr>
2023-12-11 20:33:36 +0900

    update run_sweep.sh

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+#opt=static
+for opt in static dyn; do
+cp src/cc/cc_${opt}.cc src/cc.cc
 mkdir -p log
-for ((i=12; i <=20; i++)); do
+for ((i=11; i <=20; i++)); do
   cp src/config/config_$i.h src/config.h
   make clean && make -j
 for bench in kron road twitter urand web; do
   echo "$bench,$i"
   base=log/pr
-  log=$base/${bench}_static.out
+  log=$base/${bench}_${opt}.out
   mkdir -p $base
   echo "$bench,$i" >> $log
-  #./cc -f benchmark/graphs/$bench.sg -n64 >> log/${bench}_static.out
-  ./pr -f benchmark/graphs/$bench.sg -n64 >> $log
-  echo "\n" >> $log
+  ./cc -f benchmark/graphs/$bench.sg -n64 >> log/${bench}_static.out
+  #./pr -f benchmark/graphs/$bench.sg -n64 >> $log
+  echo "" >> $log
+done
 done
 done
